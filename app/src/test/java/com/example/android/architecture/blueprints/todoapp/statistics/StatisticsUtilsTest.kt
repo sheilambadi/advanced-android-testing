f3eb528b7d848dd42887d668ce7668569af4d265
Sheila Mbadi <mbadisheila@gmail.com>
2024-09-04 20:13:09 +0300

    TDD: Write failing test then modify code to pass

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -20,7 +20,7 @@ class StatisticsUtilsTest {
     }
 
     @Test
-    fun getActiveAndCompletedStats_noActiveOneCompleted_returnsZeroHundred() {
+    fun getActiveAndCompletedStats_noActive_returnsZeroHundred() {
         val tasks = listOf<Task>(Task("title", "desc", isCompleted = true))
 
         val result = getActiveAndCompletedStats(tasks)
@@ -30,7 +30,7 @@ class StatisticsUtilsTest {
     }
 
     @Test
-    fun getActiveAndCompletedStats_twoCompletedThreeActive_returnsFortySixty() {
+    fun getActiveAndCompletedStats_both_returnsFortySixty() {
         val tasks = listOf<Task>(
             Task("title", "desc", isCompleted = true),
             Task("title", "desc", isCompleted = true),
@@ -44,4 +44,25 @@ class StatisticsUtilsTest {
         assertThat(result.activeTasksPercent, `is`(60f))
         assertThat(result.completedTasksPercent, `is`(40f))
     }
+
+    @Test
+    fun getActiveAndCompletedStats_error_returnsZero() {
+        val tasks = null
+
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent, `is`(0f))
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_empty_returnsZero() {
+        val tasks = emptyList<Task>()
+
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent, `is`(0f))
+    }
+
 }
